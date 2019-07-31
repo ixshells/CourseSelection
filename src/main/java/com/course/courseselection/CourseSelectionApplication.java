a457fc2cbfcc10b9617933a1a28607af4d233f9c
ixshells <ixshells@163.com>
2019-07-31 16:37:46 +0800

    update package name

--- src/main/java/com/course/courseselection/CourseSelectionApplication.java
+++ src/main/java/com/course/courseselection/CourseSelectionApplication.java
@@ -1,4 +1,4 @@
-package com.course.CourseSelection;
+package com.course.courseselection;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/course/courseselection/CourseSelectionApplicationTests.java
+++ src/test/java/com/course/courseselection/CourseSelectionApplicationTests.java
@@ -1,4 +1,4 @@
-package com.course.CourseSelection;
+package com.course.courseselection;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
